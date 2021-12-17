4735506b30f0d2c2c6e0e3322a9fae79bc5fa321
toriwei <toriw0208@gmail.com>
2021-12-16 17:07:28 -0800

    nav bar

--- src/components/Article.js
+++ src/components/Article.js
@@ -5,8 +5,11 @@ export default function Article({ article, remover }) {
         <p>No playlist selected</p>
       ) : (
         <section>
-          <h2>{article.title}</h2>
+          <h2>{article.movie} and {article.artist}</h2>
           <p className="date">{`Posted: ${article.date.toDate()}`}</p>
+
+          {/* <h3 className="playlist">{article.data.map(a => <p key={a.Name}>{a.Name}</p>)}</h3> */}
+
           <button
             className="buttons"
             onClick={() => {
